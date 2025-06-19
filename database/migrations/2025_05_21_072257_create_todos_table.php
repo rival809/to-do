a574e9da9e227b5d22e71e29f06325f393dc205c
Rival Ramadhan <rivalramadhan97@gmail.com>
2025-06-19 11:08:30 +0700

    feat(prod): add public

--- database/migrations/2025_05_21_072257_create_todos_table.php
+++ database/migrations/2025_05_21_072257_create_todos_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
-        Schema::table('todos', function (Blueprint $table) {
-            $table->foreignId('user_id')->after('id')->constrained()->cascadeOnDelete();
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::table('todos', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
-        });
+        Schema::dropIfExists('todos');
     }
-
 };
